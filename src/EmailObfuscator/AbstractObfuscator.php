f0b9b69519336dd6348a1895c830adcfdda6b8e5
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2015-08-21 17:00:21 +0300

    Made it possible for the obfuscator classes not to define the registerViewAssets() method if not needed.

--- src/EmailObfuscator/AbstractObfuscator.php
+++ src/EmailObfuscator/AbstractObfuscator.php
@@ -4,7 +4,10 @@ namespace Concrete\Package\AutomaticEmailObfuscator\Src\EmailObfuscator;
 abstract class AbstractObfuscator implements ObfuscatorInterface
 {
 
-    abstract public function registerViewAssets();
+    public function registerViewAssets()
+    {
+        // Register assets if needed
+    }
 
     public function obfuscateMail($email)
     {
